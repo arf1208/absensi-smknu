2a309d7fdee0720814ece7723e313bf3a7b645bf
arfdev <arsegy12@gmail.com>
2025-12-10 13:34:29 +0700

    Add footer to the main layout

---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+</main>
+        <footer>
+            <p>&copy; <?php echo date('Y'); ?> System Absensi SMK NU Lamongan | Developed by Arief Rahman</p>
+        </footer>
+    </div>
+</body>
+</html>
+<?php 
+// Tutup koneksi database saat halaman selesai
+$conn->close();
+?>
